c36bc745aebbd167b9ec17074d7725efb26792e7
ketu <brishketu.kislay@glasgow.ac.uk>
2026-08-11 19:14:42 +0100

    navigation cleanup

--- src/components/layout/navigation.ts
+++ src/components/layout/navigation.ts
@@ -2,40 +2,22 @@ import {
   LayoutDashboard,
   Boxes,
   Shield,
-  Settings,
 } from "lucide-react";
 
-
 export const navigationItems = [
-
   {
     name: "Dashboard",
     href: "/dashboard",
     icon: LayoutDashboard,
   },
-
   {
     name: "Models",
     href: "/models",
     icon: Boxes,
   },
-
   {
     name: "Admin",
     href: "/admin/users",
     icon: Shield,
   },
-
-  {
-    name: "Settings",
-    href: "/settings",
-    icon: Settings,
-  },
-  
-  {
-  name: "Users",
-  href: "/admin/users",
-  icon: Shield,
-},
-
 ];
